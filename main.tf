76866b432796a676082ef0f6173854c6eae7f740
thjonson <thjonson@cisco.com>
2023-03-16 14:23:13 -0400

    added comments to code

--- main.tf
+++ main.tf
@@ -23,22 +23,26 @@ data "aci_tenant" "tenant" {
 }
 */
 
+# Create the tenant
 resource "aci_tenant" "tenant" {
   name = var.tenant
 }
 
+# Create the VRF in the tenant
 resource "aci_vrf" "vrf" {
   tenant_dn = aci_tenant.tenant.id
   #tenant_dn = data.aci_tenant.tenant.id
   name      = var.vrf
 }
 
+# Create an application profile
 resource "aci_application_profile" "application_profile" {
   tenant_dn = aci_tenant.tenant.id
   #tenant_dn = data.aci_tenant.tenant.id
   name      = var.app_prof
 }
 
+# Loop through the CSV file to create the EPGs as requested
 resource "aci_application_epg" "application_epg" {
   for_each               = { for inst in local.instances : inst.key => inst }
   application_profile_dn = aci_application_profile.application_profile.id
@@ -46,6 +50,7 @@ resource "aci_application_epg" "application_epg" {
   relation_fv_rs_bd      = aci_bridge_domain.bridge_domain[each.key].id
 }
 
+# Loop through the CSV file to create the Bridge Domains as requested
 resource "aci_bridge_domain" "bridge_domain" {
   for_each           = { for inst in local.instances : inst.key => inst }
   tenant_dn          = aci_tenant.tenant.id
@@ -53,6 +58,8 @@ resource "aci_bridge_domain" "bridge_domain" {
   name               = each.value.bd
   relation_fv_rs_ctx = aci_vrf.vrf.id
 }
+
+# Now loop through the CSV file to assign the subnets to each of the Bridge Domains
 resource "aci_subnet" "subnet" {
   for_each  = { for inst in local.instances : inst.key => inst }
   parent_dn = aci_bridge_domain.bridge_domain[each.key].id
